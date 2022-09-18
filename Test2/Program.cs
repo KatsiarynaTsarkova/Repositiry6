62490e0ac3e93bf2ef990e95d6fb6c2872f9b1a1
Kate <katya_tsarkov@mail.ru>
2022-09-18 19:21:06 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами,
+//найдите сумму элементов, стоящих на нечетных позициях
+using static System.Console;
+int[] arRay = GetArray(7,1,9);
+WriteLine(string.Join(" ", arRay));
+ 
+    int sum = 0;
+    for (int i = 0; i < arRay.Length; i++)
+{
+    if(i %2 > 0)
+    {
+        sum = sum + arRay[i];
+    }
+}
+WriteLine($"Сумма чисел на четных позициях= {sum}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    { 
+        result[i] = new Random().Next(minValue,maxValue+1);
+    }
+    return result;
+}
